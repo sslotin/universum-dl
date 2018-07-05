9b10580be7fe58667f940ea7fc41a06a81d543ef
Sergey Slotin <slotserg@gmail.com>
2018-07-05 22:19:50 +0200

    ...

--- mnist.py
+++
@@ -1,8 +0,0 @@
-from keras.datasets import mnist
-from keras.utils import to_categorical
-
-(X, y), _ = mnist.load_data()
-X = X.reshape(60000, 784)
-X = X.astype('float32')
-X /= 255
-y = to_categorical(y, 10)
